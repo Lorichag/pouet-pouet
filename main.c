92fc917383b4a67fc2d69b4b359f8b2b3d6103f2
Lorichag <133487399+Lorichag@users.noreply.github.com>
2024-01-25 10:07:21 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "fonctions.h"
+
+int main(int argc, char ** argv){
+	FILE* fichier=NULL;
+	int* h=malloc(sizeof(int));
+	if(argv[1]="-t"){
+		fichier=fopen("tempst.csv","r");
+		if(fichier==NULL){
+			exit(1);
+		}
+		pArbre a=NULL;
+		a=T(fichier,a,h);
+		free(h);
+		fclose(fichier);
+	}
+	else if(argv[1]="-s"){
+		fichier=fopen("tempss.csv","r");
+		if(fichier==NULL){
+			exit(2);
+		}
+		pArbre2 a=NULL;
+		a=S(fichier,a,h);
+		free(h);
+		fclose(fichier);
+	}
+	return 0;
+}
