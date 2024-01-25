408fbf51e2b61bd7a60a57ae3772c7462aa64f66
Lorichag <133487399+Lorichag@users.noreply.github.com>
2024-01-25 15:26:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 int main(int argc, char ** argv){
 	FILE* fichier=NULL;
 	int* h=malloc(sizeof(int));
-	if(argv[1]="-t"){
-		fichier=fopen("tempst.csv","r");
+	if(argv[1]="-t"){ // si on lance le traitement T
+		fichier=fopen("tempst.csv","r"); // on ouvre le fichier data simplifier correspondant
 		if(fichier==NULL){
 			exit(1);
 		}
 		pArbre a=NULL;
-		a=T(fichier,a,h);
-		fclose(fichier);
+		a=T(fichier,a,h); // on appelle le code gérant le traitement T
+		fclose(fichier); 
 	}
-	else if(argv[1]="-s"){
-		fichier=fopen("tempss.csv","r");
+	else if(argv[1]="-s"){ // si on lance le traitement S
+		fichier=fopen("tempss.csv","r"); // on ouvre le fichier data simplifier correspondant
 		if(fichier==NULL){
 			exit(2);
 		}
 		pArbre2 a=NULL;
-		a=S(fichier,a,h);
+		a=S(fichier,a,h); // on appelle le code gérant le traitement S
 		fclose(fichier);
 	}
 	free(h);
