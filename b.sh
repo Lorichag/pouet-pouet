5543937ecbff89d66f9668b64ee1d1947c01e878
Cupo2fleurs <153006714+Cupo2fleurs@users.noreply.github.com>
2024-02-01 16:30:08 +0100

    Delete b.sh

--- b.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-awk -F";" 'NR>1{count[$1";"$4]+=1 ; if($2==1) {count[$1";"$3]+=1;deb[$1";"$3]=1}} END{for(line in count) print line ";" count[line]";"deb[line]}' data.csv > t.csv
-awk -F";" '{count[$2]+=1;deb[$2]+=$4} END {for(line in count) print line ";" count[line]";"deb[line]}' t.csv > tempst.csv
