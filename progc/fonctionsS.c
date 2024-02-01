55f12252d0529a881e8f7e41a044c9d2dfb4fbae
Cupo2fleurs <153006714+Cupo2fleurs@users.noreply.github.com>
2024-02-01 15:32:54 +0100

    Add files via upload

---
+++ progc/fonctionsS.c
@@ -0,0 +1,282 @@
+#include "fonctions.h"
+//strcmp
+
+int min2(int a,int b){
+//fonction qui retourne la plus petite des 2 valeurs entrées en paramètres
+	if(a<=b){
+		return a;
+	}
+	return b;
+}
+
+int max2(int a,int b){
+//fonction qui retourne la plus grande des 2 valeurs entrées en paramètres
+	if(a<=b){
+		return b;
+	}
+	return a;
+}
+
+pArbre2 creationAVL2(Trajet *t){
+//déclaration du Chainon
+	pArbre2 a = malloc(sizeof(Arbre2));
+	if(a==NULL){
+		exit(1);
+	}
+	// initialisation des paramètres de la structure d'AVL
+	a->elmt=t;
+	a->fg=NULL;
+	a->fd=NULL;
+	a->eq=0;
+	return a;
+}
+
+int filsDroit2(pArbre2 a){
+// On verifie si l'arbre a un fils droit
+	if(a==NULL){
+		return -1;
+	}
+	if(a->fd==NULL){
+		return 0;
+	}
+	return 1; 
+}
+
+int equilibre2(pArbre2 a){
+//On met à jour l'équilibre du noeud 
+	if(a==NULL){
+		return 0;
+	}
+	if(a->eq<-1 || a->eq>1){
+		return-1;
+	}
+	return 1+equilibre2(a->fg)+equilibre2(a->fd);
+}
+
+pArbre2 RG2(pArbre2 a){
+// On tourne à gauche en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	int eqa,eqp;
+	pArbre2 pivot=a->fd;
+	a->fd=pivot->fg;
+	pivot->fg=a;
+	eqa=a->eq;
+	eqp=pivot->eq;
+	a->eq=eqa-max2(eqp,0)-1;	
+	pivot->eq=min2(eqa-2,min2(eqa+eqp-2,eqp-1));
+	return pivot;
+}
+
+pArbre2 RD2(pArbre2 a){
+// On tourne à droite en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	int eqa,eqp;
+	pArbre2 pivot=a->fg;
+	a->fg=pivot->fd;
+	pivot->fd=a;
+	eqa=a->eq;
+	eqp=pivot->eq;
+	a->eq=eqa-min2(eqp,0)+1;
+	pivot->eq=max2(eqa+2,max2(eqa+eqp+2,eqp+1));
+	return pivot;
+}
+
+pArbre2 DRG2(pArbre2 a){
+// On tourne à droite puis à gauche en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	a->fd=RD2(a->fd);
+	return RG2(a);
+}
+
+pArbre2 DRD2(pArbre2 a){
+// On tourne à gauche puis à droite en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	a->fg=RG2(a->fg);
+	return RD2(a);
+}
+
+pArbre2 equilibrerAVL2(pArbre2 a){
+//Fonctions qui fait les rotations nécéssaires au sein de l'AVL
+	if(a==NULL){
+		return a;
+	}
+	if(a->eq>=2){
+		if(a->fd->eq>=0){
+			return RG2(a);
+		}
+		else{
+			return DRG2(a);
+		}
+	}
+	else if(a->eq<=-2){
+		if(a->fg->eq<=0){
+			return RD2(a);
+		}
+		else{
+			return DRD2(a);
+		}
+	}
+	return a;
+}
+
+pArbre2 insertAVL2(pArbre2 a,Trajet* t,int* h){
+//Fonction qui ajoute un trajet à l'AVL
+	if(a==NULL){
+	// si l'arbre est vide
+		*h=1;
+		a=creationAVL2(t);
+		return a;
+	}
+	else if(t->max-t->min < ((a->elmt->max)-(a->elmt->min))){
+	//si la distance max - la distance min du trajet est inférieur à celui du noeud on va à gauche
+		a->fg=insertAVL2(a->fg,t,h);
+		*h=-*h;
+	}
+	else if(t->max-t->min > a->elmt->max-a->elmt->min){
+		//si la distance max - la distance min du trajet est supérieur à celui du noeud on va à gauche
+		a->fd=insertAVL2(a->fd,t,h);
+	}
+	else{
+		//si elle est déjà dedans on ne fait rien
+		*h=0;
+		return a;
+	}
+	if(*h!=0){
+		//on modifie les valeurs d'equilibre	
+		a->eq=a->eq+*h;
+		a=equilibrerAVL2(a);	//on équilibre l'arbre si besoin
+		if(a->eq==0){
+			*h=0;
+		}
+		else{
+			*h=1;
+		}
+	}
+	return a;
+}
+
+Trajet maxAVL2(pArbre2 a){
+//Fonction qui récupère le plus grand élément de l'AVL
+	if(filsDroit2(a)==1){
+		return maxAVL2(a->fd);
+	}
+	else{
+		return *(a->elmt);
+	}
+}
+
+pArbre2 supprmaxAVL2(pArbre2 a){
+//Fonction qui supprime le plus grand élément de l'AVL
+	if(a==NULL){
+	//Si l'arbre est nul
+		return NULL;
+	}
+	if(a->fd!=NULL){
+	// si le noueud a un fils droit on suprime son fils droit
+		a->fd=supprmaxAVL2(a->fd);
+	}
+	else{
+		pArbre2 temp=a;
+		if(a->fg!=NULL){
+		// si le noeud a un fils gauche a devient son fils gauche pour liberer l'ancien emplacement de a
+			a=a->fg;
+		}
+		else{
+			free(temp);
+			return NULL;
+		}
+		free(temp);
+	}
+	return a;
+}
+
+Top2 podium2(pArbre2 a){
+//Fonction qui va renvoyer un tableau contenant les 50 premiers trajets classés par ordre distance max - distance min
+	Top2 visite;
+	visite.tab=malloc(sizeof(Trajet)*50);
+	for(int x=0;x<50;x++){
+		visite.tab[x]=maxAVL2(a);	//on stocke le premier trajet classé par ordre distance max - distance min dans le tableau
+		a=supprmaxAVL2(a);		//on supprime ensuite cette valeur de l'AVL
+	}
+	return visite;
+}
+
+Trajet* tri2(Trajet* tab){
+//Fonction qui va trier le tableau des 50 premiers trajets classés par ordre distance max - distance min pour les mettre dans l'ordre croissant des id de trajet 
+	int i,j,min;
+	for(i=0;i<50;i++){
+		min=i;	//on intialise l'indice min à l'indice de i
+		for(j=i+1;j<50;j++){
+			if(tab[j].id<tab[min].id){
+			// si le trajet à l'indice j est avant dans l'ordre croissant que celui à l'indice min 
+				min=j;	// l'indice min devient l'indice j
+			}
+		}
+		if(min!=i){	// si l'indice min n'est supérieur à celui de i
+			Trajet temp=tab[i];	// on échange de place les trajets d'indice i et min
+			tab[i]=tab[min];
+			tab[min]=temp;	
+		}
+	}
+	return tab;
+}
+
+void sauvegarde2(Top2 premier){
+//Fonction qui va ecrire les 50 premiers trajets classés par ordre distance max - distance dans un fichier csv
+	FILE* fichier=fopen("S.csv","w");	//On créer le fichier S.csv
+	if(fichier==NULL){
+		exit(2);
+	}
+	for(int i=0;i<50;i++){
+	// On écrit dans le fichier les 50 trajets
+		fprintf(fichier,"%d;%d;%f;%f;%f\n",i+1,premier.tab[i].id,premier.tab[i].moy,premier.tab[i].min,premier.tab[i].max);
+	}
+	fclose(fichier);
+}
+
+pArbre2 S(FILE* fichier,pArbre2 a,int* h){
+// La fonction principale qui va réaliser le protocole -s 	(PS:le tout début c'est à dire comment parcourir chaque ligne du fichier csv et comment prendre séparemment les valeurs séparées par des ";" à été réaliser par chatgpt mais tous le reste à été réaliser par ma personne
+	char line[1000];
+	Trajet* nouv;
+	while(fgets(line,sizeof(line),fichier) != NULL){	// on parcourt chaque ligne du fichier initial
+		nouv=malloc(sizeof(Trajet));
+		char *token = strtok(line, ";");	// token prend les valeurs séparé par des ";"
+		int numc=1;
+		nouv->moy=0;
+		nouv->max=0;
+		nouv->min=0;
+		while (token != NULL){ // tant qu'on est pas arrivé au bout de la ligne
+			if(numc==1){
+				nouv->id=atoi(token); // on copie le nom de l'id du trajet dans le paramètre id de la structure trajet	
+			}		
+			if(numc==2){
+				nouv->moy=atof(token);	 // on copie la moyenne des distances du trajet dans le parametre moy de la structure trajet	
+			}
+			if(numc==3){
+				nouv->min=atof(token);	 // on copie la distance maximale du trajet dans le parametre moy de la structure trajet
+			}
+			if(numc==4){
+				nouv->max=atof(token);	 // on copie la distance minimale du trajet dans le parametre moy de la structure trajet
+			}
+			token = strtok(NULL, ";");
+			numc++;
+		}
+		a=insertAVL2(a,nouv,h);	// On insert le trajet dans l'AVL
+	}
+	free(nouv);
+	Top2 dix;	//On crée un élément de la structure top2
+	dix.tab=malloc(sizeof(Trajet)*10); // On initialise le tableau de la structure Top2   
+	dix=podium2(a); // on rentre les valeurs dans le tableau
+	//dix.tab=tri2(dix.tab); // on tri le tableau (pas nécéssaire d'après la version 1.0.2)
+	sauvegarde2(dix); // on ecrit le tableau dans un fichier csv
+	return a;
+}
+
---
+++ progc/fonctionsT.c
@@ -0,0 +1,280 @@
+#include "fonctions.h"
+//strcmp
+
+int min(int a,int b){
+//fonction qui retourne la plus petite des 2 valeurs entrées en paramètres
+	if(a<=b){
+		return a;
+	}
+	return b;
+}
+
+int max(int a,int b){
+//fonction qui retourne la plus grande des 2 valeurs entrées en paramètres
+	if(a<=b){
+		return b;
+	}
+	return a;
+}
+
+pArbre creationAVL(Ville *v){
+//déclaration du Chainon
+	pArbre a = malloc(sizeof(Arbre));
+	if(a==NULL){
+		exit(1);
+	}
+	// initialisation des paramètres
+	a->elmt=v;
+	a->fg=NULL;
+	a->fd=NULL;
+	a->eq=0;
+	return a;
+}
+
+int filsDroit(pArbre a){
+// On verifie si a a un fils droit
+	if(a==NULL){
+		return -1;
+	}
+	if(a->fd==NULL){
+		return 0;
+	}
+	return 1; 
+}
+
+int equilibre(pArbre a){
+//On met à jour l'équilibre du noeud 
+	if(a==NULL){
+		return 0;
+	}
+	if(a->eq<-1 || a->eq>1){
+		return-1;
+	}
+	return 1+equilibre(a->fg)+equilibre(a->fd);
+}
+
+pArbre RG(pArbre a){
+// On tourne à gauche en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	int eqa,eqp;
+	pArbre pivot=a->fd;
+	a->fd=pivot->fg;
+	pivot->fg=a;
+	eqa=a->eq;
+	eqp=pivot->eq;
+	a->eq=eqa-max(eqp,0)-1;	
+	pivot->eq=min(eqa-2,min(eqa+eqp-2,eqp-1));
+	return pivot;
+}
+
+pArbre RD(pArbre a){
+// On tourne à droite en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	int eqa,eqp;
+	pArbre pivot=a->fg;
+	a->fg=pivot->fd;
+	pivot->fd=a;
+	eqa=a->eq;
+	eqp=pivot->eq;
+	a->eq=eqa-min(eqp,0)+1;
+	pivot->eq=max(eqa+2,max(eqa+eqp+2,eqp+1));
+	return pivot;
+}
+
+pArbre DRG(pArbre a){
+// On tourne à droite puis à gauche en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	a->fd=RD(a->fd);
+	return RG(a);
+}
+
+pArbre DRD(pArbre a){
+// On tourne à gauche puis à droite en faisant attention a modifier les valeurs d'équivalence si nécéssaire
+	if(a==NULL){
+		return a;
+	}
+	a->fg=RG(a->fg);
+	return RD(a);
+}
+
+pArbre equilibrerAVL(pArbre a){
+//Fonctions qui fait les rotations nécéssaires au sein de l'AVL
+	if(a==NULL){
+		return a;
+	}
+	if(a->eq>=2){
+		if(a->fd->eq>=0){
+			return RG(a);
+		}
+		else{
+			return DRG(a);
+		}
+	}
+	else if(a->eq<=-2){
+		if(a->fg->eq<=0){
+			return RD(a);
+		}
+		else{
+			return DRD(a);
+		}
+	}
+	return a;
+}
+
+pArbre insertAVL(pArbre a,Ville* v,int* h){
+//Fonction qui ajoute une ville à l'AVL
+	if(a==NULL){
+	// si l'arbre est vide
+		*h=1;
+		a=creationAVL(v);
+		return a;
+	}
+	else if(v->nbtrajet<a->elmt->nbtrajet){
+	//si le nombre de trajet est inférieur à celui du noeud on va à gauche
+		a->fg=insertAVL(a->fg,v,h);
+		*h=-*h;
+	}
+	else if(v->nbtrajet>a->elmt->nbtrajet){
+		//si le nombre de trajet est supérieur à celui du noeud on va à droite
+		a->fd=insertAVL(a->fd,v,h);
+	}
+	else{
+		//si elle est déjà dedans on ne fait rien
+		*h=0;
+		return a;
+	}
+	if(*h!=0){
+		//on modifie les valeurs d'equilibre	
+		a->eq=a->eq+*h;
+		a=equilibrerAVL(a);	//on équilibre l'arbre si besoin
+		if(a->eq==0){
+			*h=0;
+		}
+		else{
+			*h=1;
+		}
+	}
+	return a;
+}
+
+Ville maxAVL(pArbre a){
+//Fonction qui récupère le plus grand élément de l'AVL
+	if(filsDroit(a)==1){
+		return maxAVL(a->fd);
+	}
+	else{
+		return *(a->elmt);
+	}
+}
+
+pArbre supprmaxAVL(pArbre a){
+//Fonction qui supprime le plus grand élément de l'AVL
+	if(a==NULL){
+	//Si l'arbre est nul
+		return NULL;
+	}
+	if(a->fd!=NULL){
+	// si le noueud a un fils droit on suprime son fils droit
+		a->fd=supprmaxAVL(a->fd);
+	}
+	else{
+		pArbre temp=a;
+		if(a->fg!=NULL){
+		// si le noeud a un fils gauche a devient son fils gauche pour liberer l'ancien emplacement de a
+			a=a->fg;
+		}
+		else{
+			free(temp);
+			return NULL;
+		}
+		free(temp);
+	}
+	return a;
+}
+
+Top podium(pArbre a){
+//Fonction qui va renvoyer un tableau contenant les 10 villes les plus traversées 
+	Top visite;
+	visite.tab=malloc(sizeof(Ville)*10);
+	for(int x=0;x<10;x++){
+		visite.tab[x]=maxAVL(a);	//on stocke la ville la plus traversées de l'AVL dans le tableau
+		a=supprmaxAVL(a);		//on supprime ensuite cette valeur de l'AVL
+	}
+	return visite;
+}
+
+Ville* tri(Ville* tab){
+//Fonction qui va trier le tableau des 10 villes les plus traversées pour les mettre dans l'ordre alphabétique
+	int i,j,min;
+	for(i=0;i<10;i++){
+		min=i;	//on intialise l'indice min à l'indice de i
+		for(j=i+1;j<10;j++){
+			if(strcmp(tab[j].nom,tab[min].nom)<0){
+			// si la ville à l'indice j est avant dans l'ordre alphabétique que celle à l'indice min 
+				min=j;	// l'indice min devient l'indice j
+			}
+		}
+		if(min!=i){	// si l'indice min n'est supérieur à celui de i
+			Ville temp=tab[i];	// on échange de place les ville d'indice i et min
+			tab[i]=tab[min];
+			tab[min]=temp;	
+		}
+	}
+	return tab;
+}
+
+void sauvegarde(Top premier){
+//Fonction qui va ecrire les 10 villes les plus traversées dans un fichier csv
+	FILE* fichier=fopen("T.csv","w");	//On créer le fichier T.csv
+	if(fichier==NULL){
+		exit(2);
+	}
+	for(int i=0;i<10;i++){
+	// On écrit dans le fichier les 10 villes
+		fprintf(fichier,"%s;%d;%d\n",premier.tab[i].nom,premier.tab[i].nbtrajet,premier.tab[i].nbdep);
+	}
+	fclose(fichier);
+}
+
+pArbre T(FILE* fichier,pArbre a,int* h){
+// La fonction principale qui va réaliser le protocole -t 	(PS:le tout début c'est à dire comment parcourir chaque ligne du fichier csv et comment prendre séparemment les valeurs
+	// séparées par des ";" à été réaliser par chatgpt mais tous le reste à été réaliser par ma personne
+	char line[1000];
+	Ville* nouv;
+	while(fgets(line,sizeof(line),fichier) != NULL){	// on parcourt chaque ligne du fichier initial
+		nouv=malloc(sizeof(Ville));
+		char *token = strtok(line, ";");	// token prend les valeurs séparé par des ";"
+		int numc=1;
+		nouv->nbtrajet=0;
+		nouv->nbdep=0;
+		while (token != NULL){ // tant qu'on est pas arrivé au bout de la ligne
+			if(numc==1){
+				strcpy(nouv->nom,token); // on copie le nom de la ville dans le parametre nom de la structure ville	
+			}		
+			if(numc==2){
+				nouv->nbtrajet=atoi(token);	 // on copie le nombre de trajet de la ville dans le parametre nbtrajet de la structure ville	
+			}
+			if(numc==3){
+				nouv->nbdep=atoi(token);	 // on copie le nombre de fois que la ville est en départ d'étape dans le parametre nbdep de la structure ville
+			}
+			token = strtok(NULL, ";");
+			numc++;
+		}
+		a=insertAVL(a,nouv,h);	// On insert la ville dans l'AVL
+	}
+	free(nouv);
+	Top dix;	//On crée un élément de la structure top
+	dix.tab=malloc(sizeof(Ville)*10); // On initialise le tableau de la structure Top   
+	dix=podium(a); // on rentre les valeurs dans le tableau
+	dix.tab=tri(dix.tab); // on tri le tableau
+	sauvegarde(dix); // on ecrit le tableau dans un fichier csv
+	return a;
+}
+
+
